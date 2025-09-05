ced375f4940b7825bf0187f4d4d84ac4836f52ba
Canh <thuhienhc2020@gmail.com>
2025-09-05 11:32:36 +0700

    fix

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import authController from '../controllers/authController.js';
-import auth from '../middlewares/auth.js';
+import express from "express";
+import authController from "../controllers/authController.js";
+import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.get('/profile', auth, authController.getProfile);
-router.post('/refresh-token', authController.refreshToken);
-router.get('/facebook', authController.loginWithFacebook);
-router.get('/facebook/callback', authController.facebookCallback);
-
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/profile", auth, authController.getProfile);
+router.post("/refresh-token", authController.refreshToken);
+router.get("/facebook", authController.loginWithFacebook);
+router.get("/facebook/callback", authController.facebookCallback);
 
 export default router;
